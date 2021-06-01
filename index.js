c7374b8bfe166897c85feb97f3b9b059b118b449
Владислав Ефимов <vladislavich2@gmail.com>
2021-06-02 00:49:19 +0700

    changed URL

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 
-var app4 =   new Vue({
+var app4 = new Vue({
     el: '#app4',
     data() {
-      return {
-        info: null
-      };
+        return {
+            info: null
+        };
     },
-    props:{
+    props: {
         currPage: Array,
-        currPageNumber:{
-            type:Number,
-            default:1
+        currPageNumber: {
+            type: Number,
+            default: 1
         },
         pageCount: Number
 
     },
     methods: {
-        setSecond: function(event)
-        {
+        setSecond: function (event) {
             axios
-            .get('https://localhost:5001/api/Products/get/page/2')
-            .then(response => (this.currPage= response));
-            
+                .get('http://localhost:62233/api/Products/get/page/2')
+                .then(response => (this.currPage = response));
+
         },
-        setPage: function(pageNumber)
-        {
+        setPage: function (pageNumber) {
             axios
-            .get('https://localhost:5001/api/Products/get/page/'+pageNumber)
-            .then(response => (this.currPage= response));
+                .get('http://localhost:62233/api/Products/get/page/' + pageNumber)
+                .then(response => (this.currPage = response));
 
         }
     },
     mounted() {
-      axios
-        .get('https://localhost:5001/api/Products/get/page/'+this.currPageNumber)
-        .then(response => (this.currPage= response));
+        axios
+            .get('http://localhost:62233/api/Products/get/page/' + this.currPageNumber)
+            .then(response => (this.currPage = response));
     },
-    created(){
+    created() {
         axios
         axios
-        .get('https://localhost:5001/api/Products/count')
-        .then(response => (this.pageCount = Math.ceil(response.data / 12)));
+            .get('http://localhost:62233/api/Products/count')
+            .then(response => (this.pageCount = Math.ceil(response.data / 12)));
 
     }
-  })
+})
 var app5 = new Vue({
     el: '#app5',
-    data(){
-        return{
+    data() {
+        return {
             pages: 1
         };
     },
-    mounted(){
+    mounted() {
         axios
-        .get('https://localhost:5001/api/Products/count')
-        .then(response => (this.pages = response.data / 12));
+            .get('http://localhost:62233/api/Products/count')
+            .then(response => (this.pages = response.data / 12));
     }
 })
 

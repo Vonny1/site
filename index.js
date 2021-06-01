0f41b49f21282990acb9aaa6b1f8d75e8965e689
Ефимов Владислав Сергеевич <Vladislav.Efimov@main.russianpost.ru>
2021-06-01 15:03:40 +0700

    trying

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ var app4 =   new Vue({
         info: null
       };
     },
+    props:{
+        currPage: Array,
+        currPageNumber:{
+            type:Number,
+            default:1
+        }
+    },
+    methods: {
+        getPage(number)
+        {
+            axios
+            .get('https://localhost:5001/api/Products/get/page/${number}')
+            .then(response => (this.info = response));
+        }
+
+    },
     mounted() {
       axios
         .get('https://localhost:5001/api/Products/get/page/1')
-        .then(response => (this.info = response));
+        .then(response => (this.info= response));
     }
   })
+var app5 = new Vue({
+    el: '#app5',
+    data(){
+        return{
+            pages: 10
+        };
+    },
+    mounted(){
+        axios
+        .get('https://localhost:5001/api/Products/count')
+        .then(response => (this.pages = response.data / 12));
+    }
+})
 

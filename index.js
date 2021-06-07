2d6db4e5e046a6ffc252c0de8bcf5c63665e05a0
Владислав Ефимов <vladislavich2@gmail.com>
2021-06-08 03:28:35 +0700

    added component

--- index.js
+++ index.js
@@ -1,58 +1,69 @@
 
-var app4 = new Vue({
-    el: '#app4',
-    data() {
-        return {
-            info: null
-        };
+
+Vue.component('productvue',
+{
+  props: {
+    id: Number,
+    name: String,
+    cost: Number,
+    categoryid: Number,
+    category: {
+      id: Number,
+      name: Number
+    }
+  },
+  methods: {
+    alertName: function (event) {
+      alert(this.name);
+    }
     },
-    props: {
-        currPage: Array,
-        currPageNumber: {
-            type: Number,
-            default: 1
-        },
-        pageCount: Number
+}
+);
+
+
+new Vue({
+  el: '#app4',
+  data() {
+    return {
+      info: null
+    };
+  },
+  props: {
+    currPage: Array,
+    currPageNumber: {
+      type: Number,
+      default: 1
+    },
+    pageCount: Number
+
+  },
+  methods: {
+    setSecond: function (event) {
+      axios
+        .get('http://localhost:62233/api/Products/get/page/2')
+        .then(response => (this.currPage = response));
 
     },
-    methods: {
-        setSecond: function (event) {
-            axios
-                .get('http://localhost:62233/api/Products/get/page/2')
-                .then(response => (this.currPage = response));
-
-        },
-        setPage: function (pageNumber) {
-            axios
-                .get('http://localhost:62233/api/Products/get/page/' + pageNumber)
-                .then(response => (this.currPage = response));
-
-        }
-    },
-    mounted() {
-        axios
-            .get('http://localhost:62233/api/Products/get/page/' + this.currPageNumber)
-            .then(response => (this.currPage = response));
-    },
-    created() {
-        axios
-        axios
-            .get('http://localhost:62233/api/Products/count')
-            .then(response => (this.pageCount = Math.ceil(response.data / 12)));
+    setPage: function (pageNumber) {
+      axios
+        .get('http://localhost:62233/api/Products/get/page/' + pageNumber)
+        .then(response => (this.currPage = response));
 
     }
+
+  },
+  mounted() {
+    axios
+      .get('http://localhost:62233/api/Products/get/page/' + this.currPageNumber)
+      .then(response => (this.currPage = response));
+  },
+  created() {
+    axios
+    axios
+      .get('http://localhost:62233/api/Products/count')
+      .then(response => (this.pageCount = Math.ceil(response.data / 12)));
+
+  }
 })
-var app5 = new Vue({
-    el: '#app5',
-    data() {
-        return {
-            pages: 1
-        };
-    },
-    mounted() {
-        axios
-            .get('http://localhost:62233/api/Products/count')
-            .then(response => (this.pages = response.data / 12));
-    }
-})
+
 

2e569ca7cedffae6e515d9af2eeed5c8828d5377
Ефимов Владислав Сергеевич <Vladislav.Efimov@main.russianpost.ru>
2021-06-09 16:53:57 +0700

    push

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ Vue.component('productmodal',
                       <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                      ...
+                      test
                   </div>
                   <div class="modal-footer">
                       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>

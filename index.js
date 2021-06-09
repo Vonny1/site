72e125d13a90a1865619bde5da617f5d2f4aacf0
Ефимов Владислав Сергеевич <Vladislav.Efimov@main.russianpost.ru>
2021-06-09 16:24:36 +0700

    modal opening from cards

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
 
-
 Vue.component('productvue',
-{
-  props: {
-    id: Number,
-    name: String,
-    cost: Number,
-    categoryid: Number,
-    category: {
-      id: Number,
-      name: Number
-    }
-  },
-  methods: {
-    alertName: function (event) {
-      alert(this.name);
-    }
+  {
+    // props: {
+    //   id: Number,
+    //   name: String,
+    //   cost: Number,
+    //   categoryid: Number,
+    //   category: {
+    //     id: Number,
+    //     name: Number
+    //   }
+    // },
+    props: {
+      productobj: Object
+
     },
-}
+    methods: {
+      alertName: function (event) {
+        alert(this.productobj.name);
+      }
+    },
+  }
 );
+Vue.component('productmodal',
+  {
+    props: {
+      productmodalobj: Object
+    },
+    template:
+      `
+      <div class="modalcont">
+      <div class="modal " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                      <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                      ...
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+  `
+  }
+)
 
 
 new Vue({
@@ -40,13 +73,13 @@ new Vue({
   methods: {
     setSecond: function (event) {
       axios
-        .get('http://localhost:62233/api/Products/get/page/2')
+        .get('https://localhost:5001/api/Products/get/page/2')
         .then(response => (this.currPage = response));
 
     },
     setPage: function (pageNumber) {
       axios
-        .get('http://localhost:62233/api/Products/get/page/' + pageNumber)
+        .get('https://localhost:5001/api/Products/get/page/' + pageNumber)
         .then(response => (this.currPage = response));
 
     }
@@ -54,13 +87,13 @@ new Vue({
   },
   mounted() {
     axios
-      .get('http://localhost:62233/api/Products/get/page/' + this.currPageNumber)
+      .get('https://localhost:5001/api/Products/get/page/' + this.currPageNumber)
       .then(response => (this.currPage = response));
   },
   created() {
     axios
     axios
-      .get('http://localhost:62233/api/Products/count')
+      .get('https://localhost:5001/api/Products/count')
       .then(response => (this.pageCount = Math.ceil(response.data / 12)));
 
   }
